f7a76194cb994d3b5fe67ad8c61ff194afb8c08a
Nabil Khalid Alkaff <nabilkhalid.dev@gmail.com>
2021-04-14 07:30:03 +0800

    dev branch

--- pages/four.vue
+++ pages/four.vue
@@ -4,8 +4,7 @@
     title="prayerWidget"
     class="widget-m-top"
     style="height: 358px; border: 1px solid #ddd"
-    scrolling="no"
-    src="https://www.islamicfinder.org/prayer-widget/1735158/shafi/13/0/20.0/18.0"
+    scrolling="no" src="https://www.islamicfinder.org/prayer-widget/1735158/shafi/13/0/20.0/18.0"
   >
   </iframe>
 </template>
